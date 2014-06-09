9728cd8e1d4b0df2d9e871296f5ac1aa637f720f
Rashmith <rashmith@rashmith-Inspiron-N5050.(none)>
2014-06-09 11:58:39 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BackboneMarionette::Application.config.secret_key_base = '546fd5987d56ff9c615a75d9c04dad168a239578bd7c8004ccf4264f191809222c7ea65bacddb202c194c226210061bb50c68d1569e7b49b2ba2354b9ab29786'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BackboneMarionette::Application.config.session_store :cookie_store, key: '_backbone-marionette_session'
